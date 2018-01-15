974f86c970fe18c2896e977a960c7a8812bcdea0
WildQnex <konkord321@gmail.com>
2018-01-15 14:56:33 +0300

    Introduce Reservation and Status entities

---
+++ src/main/java/by/martyniuk/hotelbooking/entity/Reservation.java
@@ -0,0 +1,28 @@
+package by.martyniuk.hotelbooking.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation implements Serializable, Cloneable{
+    private long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private LocalTime orderTime;
+    private int personAmount;
+    private BigDecimal costPerPerson;
+    private BigDecimal costPerNight;
+    private BigDecimal animalCost;
+    private BigDecimal totalCost;
+    private User user;
+    private Apartment apartment;
+    private Status status;
+}
---
+++ src/main/java/by/martyniuk/hotelbooking/entity/Status.java
@@ -0,0 +1,7 @@
+package by.martyniuk.hotelbooking.entity;
+
+public enum Status {
+    WAITING_FOR_APPROVE,
+    APPROVED,
+    DECLINED
+}
